8886ced487ed2851e97239cda9e04bf8e0393b36
Polad <bakhip@usi.ch>
2024-08-23 16:45:20 +0200

    get daily data yahoo, construct yahoo url

--- price_retrieval.py
+++ price_retrieval.py
@@ -21,3 +21,23 @@ def obtain_list_of_db_tickers():
         cur.execute("SELECT id, ticker FROM symbol")
         data = cur.fetchall()
         return [(d[0], d[1]) for d in data]
+
+def get_daily_historic_data_yahoo(
+        ticker, start_date=(2000,1,1),
+        end_date=datetime.date.today().timetuple()[0:3] 
+    ):
+    """
+    Obtains data from Yahoo Finance returns and a list of tuples.
+    ticker: Yahoo Finance ticker symbol, e.g. "GOOG" for Google, Inc. start_date: Start date in (YYYY, M, D) format
+    end_date: End date in (YYYY, M, D) format
+    """
+    # Construct the Yahoo URL with the correct integer query parameters 
+    # for start and end dates. Note that some parameters are zero-based!
+    ticker_tup = (
+        ticker, start_date[1]-1, start_date[2],
+        start_date[0], end_date[1]-1, end_date[2],
+        end_date[0]
+    )
+    yahoo_url = "http://ichart.finance.yahoo.com/table.csv"
+    yahoo_url += "?s=%s&a=%s&b=%s&c=%s&d=%s&e=%s&f=%s"
+    yahoo_url = yahoo_url % ticker_tup
