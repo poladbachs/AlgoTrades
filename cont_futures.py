7c242e62f71d181f77bdcffa87644e26a0dd6233
Polad <bakhip@usi.ch>
2024-08-25 00:14:09 +0200

    created weightings for futures contract, matrix produced

--- cont_futures.py
+++ cont_futures.py
@@ -24,4 +24,20 @@ def futures_rollover_weights(start_date, expiry_dates, contracts, rollover_days=
                                 index=dates, columns=contracts)
     prev_date = roll_weights.index[0]
 
-    
+    # Loop through each contract and create the specific weightings for 
+    # each contract depending upon the settlement date and rollover_days 
+    for i, (item, ex_date) in enumerate(expiry_dates.iteritems()):
+        if i < len(expiry_dates) - 1:
+            roll_weights.ix[prev_date:ex_date - pd.offsets.BDay(), item] = 1 
+            roll_rng = pd.date_range(end=ex_date - pd.offsets.BDay(),
+                                     periods=rollover_days + 1, freq='B')
+
+            # Create a sequence of roll weights (i.e. [0.0,0.2,...,0.8,1.0] 
+            # and use these to adjust the weightings of each future 
+            decay_weights = np.linspace(0, 1, rollover_days + 1) 
+            roll_weights.ix[roll_rng, item] = 1 - decay_weights 
+            roll_weights.ix[roll_rng, expiry_dates.index[i+1]] = decay_weights
+        else:
+            roll_weights.ix[prev_date:, item] = 1
+        prev_date = ex_date
+    return roll_weights
