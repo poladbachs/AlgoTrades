6cf50add5f85d7ef6e3cc7d3387293526bd629f8
Polad <bakhip@usi.ch>
2024-08-24 22:24:58 +0200

    downloading contract from nasdaq data, constructed api call

--- quandl_data.py
+++ quandl_data.py
@@ -21,3 +21,16 @@ def construct_futures_symbols(symbol, start_year=2020, end_year=2024):
             futures.append("%s%s%s" % (symbol, m, y))
     return futures
 
+# Function to download a contract from Nasdaq Data Link
+def download_contract_from_quandl(contract, dl_dir, api_key):
+    """
+    Download an individual futures contract from Nasdaq Data Link and then
+    store it to disk in the 'dl_dir' directory.
+    """
+    # Construct the API call from the contract and api_key
+    api_call = f"https://data.nasdaq.com/api/v3/datasets/CHRIS/CME_{contract}.csv"
+    params = f"?api_key={api_key}&order=asc"
+    full_url = f"{api_call}{params}"
+
+    # Download the data from Nasdaq Data Link
+    response = requests.get(full_url)
