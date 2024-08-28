ff53e6cc7a50e1a23829f8b81621c684ab5bf9d8
Polad <bakhip@usi.ch>
2024-08-28 05:31:02 +0200

    plotted residual values from linear model of two prices

--- cadf.py
+++ cadf.py
@@ -31,3 +31,18 @@ def plot_scatter_series(df, ts1, ts2):
     plt.title('%s and %s Price Scatterplot' % (ts1, ts2))
     plt.scatter(df[ts1], df[ts2])
     plt.show()
+
+def plot_residuals(df):
+    months = mdates.MonthLocator() # every month fig, ax = plt.subplots()
+    ax.plot(df.index, df["res"], label="Residuals") 
+    ax.xaxis.set_major_locator(months) 
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y')) 
+    ax.set_xlim(datetime.datetime(2012, 1, 1), datetime.datetime(2013, 1, 1)) 
+    ax.grid(True)
+    fig.autofmt_xdate()
+    plt.xlabel('Month/Year')
+    plt.ylabel('Price ($)')
+    plt.title('Residual Plot')
+    plt.legend()
+    plt.plot(df["res"])
+    plt.show()
