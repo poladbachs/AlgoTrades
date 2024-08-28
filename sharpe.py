d235b1a4c88305755fbfcafb76fc4516a6bfcccf
Polad <bakhip@usi.ch>
2024-08-28 18:16:36 +0200

    calculated sharpe ratio for GOOG over 2010-2023 to assess its performance over risk taken

--- sharpe.py
+++ sharpe.py
@@ -21,10 +21,21 @@ def equity_sharpe(ticker):
 
     The dates have been hardcoded here for brevity. 
     """
-    start = datetime.datetime(2020,1,1)
+    start = datetime.datetime(2010,1,1)
     end = datetime.datetime(2023,1,1)
 
     # Obtain the equities daily historic data for the desired time period 
     # and add to a pandas DataFrame
-    pdf = yf.download(ticker, 'google', start=start, end=end)
+    pdf = yf.download(ticker, start=start, end=end)
 
+    # Use the percentage change method to easily calculate daily returns
+    pdf['daily_ret'] = pdf['Close'].pct_change()
+
+    # Assume an average annual risk-free rate over the period of 5%
+    pdf['excess_daily_ret'] = pdf['daily_ret'] - 0.05/252
+
+    # Return the annualised Sharpe ratio based on the excess daily returns
+    return annualised_sharpe(pdf['excess_daily_ret'])
+
+# For Google, the Sharpe ratio for buying and holding is:
+print(equity_sharpe('GOOG'))
