1109dfd59b5d4978ecbded6126d602ef9668a52a
Polad <bakhip@usi.ch>
2024-08-28 02:46:18 +0200

    created brownian motion, mean revert and trending

--- hurst_exponent.py
+++ hurst_exponent.py
@@ -16,3 +16,9 @@ def hurst(ts):
 
     # Return the Hurst exponent from the polyfit output
     return poly[0]*2.0
+
+# Create a Gometric Brownian Motion, Mean-Reverting and Trending Series
+gbm = log(cumsum(randn(100000))+1000)
+mr = log(randn(100000)+1000)
+tr = log(cumsum(randn(100000)+1)+1000)
+
