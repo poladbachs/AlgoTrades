8c666866867f75c57adcd2cd77c13e9fb80b4b35
Polad <bakhip@usi.ch>
2024-08-29 21:15:53 +0200

    data handler abstract class: added get latest bar

---
+++ Event-Driven Trading Engine/data.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+from abc import ABCMeta, abstractmethod 
+import datetime
+import os, os.path
+import numpy as np 
+import pandas as pd
+from event import MarketEvent
+
+class DataHandler(object): 
+    """
+    DataHandler is an abstract base class providing an interface 
+    for all subsequent (inherited) data handlers (both live and historic).
+    The goal of a (derived) DataHandler object is to output 
+    a generated set of bars (OHLCVI) for each symbol requested.
+    This will replicate how a live strategy would function as current
+    market data would be sent "down the pipe". Thus a historic and live system 
+    will be treated identically by the rest of the backtesting suite. 
+    """
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def get_latest_bar(self, symbol):
+        """
+        Returns the last bar updated.
+        """
+        raise NotImplementedError("Should implement get_latest_bar()")
