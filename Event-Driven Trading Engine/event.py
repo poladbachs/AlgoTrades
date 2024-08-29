a651ad0c843ea48da509d65aa8a8c9518a93139e
Polad <bakhip@usi.ch>
2024-08-29 20:13:28 +0200

    adding OrderEvent class to send Order to execution

--- Event-Driven Trading Engine/event.py
+++ Event-Driven Trading Engine/event.py
@@ -46,3 +46,24 @@ class SignalEvent(Event):
         self.datetime = datetime
         self.signal_type = signal_type
         self.strength = strength
+
+class OrderEvent(Event): 
+    """
+    Handles the event of sending an Order to an execution system.
+    The order contains a symbol (e.g. GOOG), a type (market or limit),
+    quantity and a direction.
+    """
+
+    def __init__(self, symbol, order_type, quantity, direction):
+        """
+        Initialises the order type, setting whether it is
+        a Market order ('MKT') or Limit order ('LMT'), has
+        a quantity (integral) and its direction ('BUY' or
+        'SELL').
+
+        Parameters:
+        symbol - The instrument to trade.
+        order_type - 'MKT' or 'LMT' for Market or Limit.
+        quantity - Non-negative integer for quantity.
+        direction - 'BUY' or 'SELL' for long or short.
+        """
