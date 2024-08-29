200799adbdde07331d0cfeed780cc95df29004fb
Polad <bakhip@usi.ch>
2024-08-29 21:05:19 +0200

    calculated Interactive Brokers fee of trading

--- Event-Driven Trading Engine/event.py
+++ Event-Driven Trading Engine/event.py
@@ -115,4 +115,18 @@ class FillEvent(Event):
         else:
             self.commission = commission
 
-        
+    def calculate_ib_commission(self):
+        """
+        Calculates the fees of trading based on an Interactive
+        Brokers fee structure for API, in USD.
+        This does not include exchange or ECN fees.
+        Based on "US API Directed Orders": 
+        https://www.interactivebrokers.com/en/index.php? 
+        f=commission&p=stocks2
+        """
+        full_cost = 1.3
+        if self.quantity <= 500:
+            full_cost = max(1.3, 0.013 * self.quantity)
+        else: # Greater than 500
+            full_cost = max(1.3, 0.008 * self.quantity)
+        return full_cost
