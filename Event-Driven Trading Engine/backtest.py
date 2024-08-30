aac8f2a3cb48c82167242959bb22a152e792b734
Polad <bakhip@usi.ch>
2024-08-30 22:55:18 +0200

    handled all the events, market, signal, order and fill

--- Event-Driven Trading Engine/backtest.py
+++ Event-Driven Trading Engine/backtest.py
@@ -81,3 +81,26 @@ class Backtest(object):
                 self.data_handler.update_bars()
             else: 
                 break
+            
+            # Handle the events
+            while True: 
+                try:
+                    event = self.events.get(False)
+                except queue.Empty:
+                    break
+                else:
+                    if event is not None:
+                        if event.type == 'MARKET': 
+                            self.strategy.calculate_signals(event) 
+                            self.portfolio.update_timeindex(event)
+                        elif event.type == 'SIGNAL': 
+                            self.signals += 1 
+                            self.portfolio.update_signal(event)
+                        elif event.type == 'ORDER': 
+                            self.orders += 1
+                            self.execution_handler.execute_order(event)
+                        elif event.type == 'FILL': 
+                            self.fills += 1
+                            self.portfolio.update_fill(event)
+
+            time.sleep(self.heartbeat)
