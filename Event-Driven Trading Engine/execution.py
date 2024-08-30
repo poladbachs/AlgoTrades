2c12eac78763c9c506d2f93b3960cb614a700eb2
Polad <bakhip@usi.ch>
2024-08-30 22:10:14 +0200

    implemented execute order function, converting order objects into fill objects without latency

--- Event-Driven Trading Engine/execution.py
+++ Event-Driven Trading Engine/execution.py
@@ -50,3 +50,17 @@ class SimulatedExecutionHandler(ExecutionHandler):
         events - The Queue of Event objects.
         """
         self.events = events
+
+    def execute_order(self, event): 
+        """
+        Simply converts Order objects into Fill objects naively,
+        i.e. without any latency, slippage or fill ratio problems.
+        Parameters:
+        event - Contains an Event object with order information. 
+        """
+        if event.type == 'ORDER':
+            fill_event = FillEvent(
+                datetime.datetime.utcnow(), event.symbol,
+                'ARCA', event.quantity, event.direction, None
+            )
+            self.events.put(fill_event)
