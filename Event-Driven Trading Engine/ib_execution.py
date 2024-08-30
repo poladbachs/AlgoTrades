95dbf7f129409627573aca30958bae0219f1c4ed
Polad <bakhip@usi.ch>
2024-08-31 00:01:02 +0200

    Interactive Brokers Execution Handler: init function added

---
+++ Event-Driven Trading Engine/ib_execution.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+import datetime 
+import time
+
+from ib.ext.Contract import Contract 
+from ib.ext.Order import Order
+from ib.opt import ibConnection, message
+
+from event import FillEvent, OrderEvent 
+from execution import ExecutionHandler
+
+class IBExecutionHandler(ExecutionHandler): 
+    """
+    Handles order execution via the Interactive Brokers
+    API, for use against accounts when trading live
+    directly.
+    """
+    def __init__(
+        self, events, order_routing="SMART", currency="USD" 
+    ):
+        """
+        Initialises the IBExecutionHandler instance. 
+        """
+        self.events = events
+        self.order_routing = order_routing
+        self.currency = currency
+        self.fill_dict = {}
+        self.tws_conn = self.create_tws_connection() 
+        self.order_id = self.create_initial_order_id()
+        self.register_handlers()
