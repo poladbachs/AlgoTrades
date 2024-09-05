11902497f166ad19785f99072f1099d27b5be6e1
Polad <bakhip@usi.ch>
2024-09-05 04:33:15 +0200

    started mean reverting equity pairs trade strategy

---
+++ trading_engine/intraday_mr.py
@@ -0,0 +1,13 @@
+from __future__ import print_function 
+
+import datetime
+import numpy as np 
+import pandas as pd
+import statsmodels.api as sm
+
+from strategy import Strategy
+from event import SignalEvent
+from backtest import Backtest
+from hft_data import HistoricCSVDataHandlerHFT
+from hft_portfolio import PortfolioHFT
+from execution import SimulatedExecutionHandler
