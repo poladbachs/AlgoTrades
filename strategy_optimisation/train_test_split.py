0e0158039fd6c9d6f6514a7caca66b014642d18c
Polad <bakhip@usi.ch>
2024-09-10 20:28:04 +0200

    created param models and outputed hit rate and confus matrix for each model

--- strategy_optimisation/train_test_split.py
+++ strategy_optimisation/train_test_split.py
@@ -30,3 +30,36 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.8, random_state=42
     )
+
+    # Create the (parametrised) models
+    print("Hit Rates/Confusion Matrices:\n")
+    models = [("LR", LogisticRegression()), 
+              ("LDA", LDA()), 
+              ("QDA", QDA()),
+              ("LSVC", LinearSVC(dual=False)),
+              ("RSVM", SVC(
+              	C=1000000.0, cache_size=200, class_weight=None,
+                coef0=0.0, degree=3, gamma=0.0001, kernel='rbf',
+                max_iter=-1, probability=False, random_state=None,
+                shrinking=True, tol=0.001, verbose=False)
+              ),
+              ("RF", RandomForestClassifier(
+              	n_estimators=1000, criterion='gini', 
+                max_depth=None, min_samples_split=2, 
+                min_samples_leaf=1, max_features='sqrt',
+                bootstrap=True, oob_score=False, n_jobs=-1,
+                random_state=None, verbose=0)
+              )]
+
+    # Iterate through the models
+    for m in models:
+        
+        # Train each of the models on the training set
+        m[1].fit(X_train, y_train)
+
+        # Make an array of predictions on the test set
+        pred = m[1].predict(X_test)
+
+        # Output the hit-rate and the confusion matrix for each model
+        print("%s:\n%0.3f" % (m[0], m[1].score(X_test, y_test)))
+        print("%s\n" % confusion_matrix(pred, y_test))
